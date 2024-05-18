f73a846ccdd54d9dcb991b9cb4350ecb6ab719be
2004Re1 <revanaxun559@gmail.com>
2024-05-18 20:16:17 +0000

    new commit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         number = -1 * number
-    new = number%10
-    print(new, end ="")
+
+    new = number % 10
+    print(new, end = "")
     return(new)
