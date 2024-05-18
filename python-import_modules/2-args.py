bd6d74c554a1bdee948424f6cd0e206087f3b4b8
2004Re1 <revanaxun559@gmail.com>
2024-05-18 20:47:44 +0000

    new commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
-    args = len(argv) - 1
-    print("{} arguments:".format(args))
-    pos = 1
-    while (args > pos):
-        print("{}: {}".format(pos, argv[pos]))
-        pos = pos + 1
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
