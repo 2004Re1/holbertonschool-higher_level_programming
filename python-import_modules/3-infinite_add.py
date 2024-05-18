eb3fb44328c60df5d3f20ce9c7c9f652cee96984
2004Re1 <revanaxun559@gmail.com>
2024-05-18 21:00:07 +0000

    new commit

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,6 @@ from sys import argv
 
 if __name__ == "__main__":
     sum = 0
-	args = len(argv) - 1
-    for i in range(args):
-		sum = sum + int(argv[i])
-	print("{}".format(sum))
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{}".format(sum))
