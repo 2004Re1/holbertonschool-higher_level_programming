85a0ab83bdef7af9ace3d93d1a05e615c3027ce6
2004Re1 <revanaxun559@gmail.com>
2024-05-18 20:42:30 +0000

    new commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-args = len(sys.argv) - 1
+from sys import argv
+args = len(argv) - 1
 print("{} arguments:".format(args))
 pos = 1
 while (args > pos):
-    print("{}: {}".format(pos, sys.argv[pos]))
+    print("{}: {}".format(pos, argv[pos]))
     pos = pos + 1
