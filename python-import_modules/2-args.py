81279bf024e37ada9677ea78dc35cba2395037f3
2004Re1 <revanaxun559@gmail.com>
2024-05-18 20:38:40 +0000

    new commit

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+args = len(sys.argv) - 1
+print("{} arguments:".format(args))
+pos = 1
+while (args > pos):
+    print("{}: {}".format(pos, sys.argv[pos]))
+    pos = pos + 1
