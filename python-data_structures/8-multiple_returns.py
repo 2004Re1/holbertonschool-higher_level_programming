65158b69f4b1c28c4c471a77198d98958dd8da33
2004Re1 <revanaxun559@gmail.com>
2024-05-25 10:48:18 +0000

    new commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 def multiple_returns(sentence):
     # Check if the sentence is empty
     if not sentence:
-        return (0, None)  # Return a tuple with length 0 and first character None
+        return (0, None)  
     else:
         return (len(sentence), sentence[0])
