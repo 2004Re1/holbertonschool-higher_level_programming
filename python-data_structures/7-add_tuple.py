8bfba92341931a67afb50ffc407cc7710ceed8b1
2004Re1 <revanaxun559@gmail.com>
2024-05-25 10:43:28 +0000

    new commit

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-  result = tuple(a + b for a, b in zip(tuple_a, tuple_b))
-  return result
+    # Ensure both tuples have at least 2 elements by adding zeros if necessary
+    tuple_a = (tuple_a + (0, 0))[:2]
+    tuple_b = (tuple_b + (0, 0))[:2]
+    # Add the corresponding elements of the tuples
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
