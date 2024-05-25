7fde05d16d8feaf12db99777a90ce5a72ab036be
2004Re1 <revanaxun559@gmail.com>
2024-05-25 19:48:28 +0000

    new commit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    # Initialize variables to keep track of the best score and corresponding key
     best_key = None
-    best_score = float('-inf')  # Initialize with negative infinity to handle negative scores
+    best_score = float('-inf')
     
-    # Iterate over the dictionary to find the key with the highest score
     for key, value in a_dictionary.items():
         if value > best_score:
             best_score = value
             best_key = key
-    
+    if best_key == None
+    return None
+
     return best_key
