643a2f93b8eafa312144027f4552ba66ffd06316
2004Re1 <revanaxun559@gmail.com>
2024-05-25 19:26:41 +0000

    new commit

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+
 def simple_delete(a_dictionary, key=""):
-  for i in a_dictionary:
-    if i is not key:
-      return a_dictionary
-  del a_dictionary[key]
-  return a_dictionary
-
-
+    # Check if the key exists in the dictionary before deleting
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
