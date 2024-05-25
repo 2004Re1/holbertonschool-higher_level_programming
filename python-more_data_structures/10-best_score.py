fc4cf134d587d2c8bb8a90ce6cf7a86044078f89
2004Re1 <revanaxun559@gmail.com>
2024-05-25 19:51:55 +0000

    new commit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -8,7 +8,4 @@ def best_score(a_dictionary):
         if value > best_score:
             best_score = value
             best_key = key
-    if best_key == None
-    return None
-
     return best_key
