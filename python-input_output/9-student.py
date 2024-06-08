8e1ed6654425ff2767aed498c3827aba25583612
2004Re1 <revanaxun559@gmail.com>
2024-06-08 14:11:25 +0000

    new commit

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""json"""
+"""JSON serialization of an object"""
 
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-class student:
-  
-  def __init__(self, first_name, last_name, age):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def class_to_json(self):
+        """Returns the dictionary description for JSON serialization of an object"""
+        j_dict = {}
 
+        for attr, value in self.__dict__.items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                j_dict[attr] = value
 
-  def class_to_json(self):
-      """Initialize an empty dictionary"""
-      j_dict = {}
-
-      for attr, value in self.__dict__.items():
-          if isinstance(value, (list, dict, str, int, bool)):
-              j_dict[attr] = value
-
-      return j_dict
+        return j_dict
