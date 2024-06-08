38da11eaa6a282a2135812b9b4f21d7abe80af6e
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-06-08 18:45:06 +0400

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def append_write(filename="", text=""):
-    with open(filename, "a") as f:
-        f.write(text)
-        f.write("\n")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, "a", encoding="utf-8") as f:
+        txt = f.write(text)
+        return txt
