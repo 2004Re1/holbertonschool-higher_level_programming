15165378292b15f41d8d2eca74af27bbe859ea7a
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-06-08 12:46:35 +0400

    Update 4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import json 
+"""Defines a string-to-JSON function."""
+import json
+
+
 def from_json_string(my_str):
-  return json.loads(my_str)
+    """Return the JSON representation of a string object."""
+    return json.loads(my_str)
