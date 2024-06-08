aa76afc73ea4f0b6f21c2817dae22621fe7249b4
2004Re1 <revanaxun559@gmail.com>
2024-06-08 14:15:23 +0000

    new commit

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""JSON serialization of a Student object"""
+"""json"""
+
 
 class Student:
+    """Class student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
+    def to_json(self):
+        j_dict = {}
 
-        If attrs is a list of strings, only attribute names contained in this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
-        """
-        if attrs is None:
-            return self.__dict__
-        
-        if not isinstance(attrs, list):
-            return self.__dict__
-        
-        for item in attrs:
-            if not isinstance(item, str):
-                return self.__dict__
-        
-        return {key: value for key, value in self.__dict__.items() if key in attrs
+        for attr, self in self.__dict__.items():
+            if isinstance(self, (list, dict, str, int, bool)):
+                j_dict[attr] = self
+
+        return j_dict
