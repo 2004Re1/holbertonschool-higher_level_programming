a08052a1b704da18e83bf03c8ece84dd0fba0ffb
2004Re1 <revanaxun559@gmail.com>
2024-06-26 00:39:17 +0400

    new commit

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,7 @@
 -- Step 1: Backup your database (optional but highly recommended)
 -- mysqldump -u username -p hbtn_0c_0 > hbtn_0c_0_backup.sql
+-- Select the database hbtn_0c_0
+USE hbtn_0c_0;
 
 -- Step 2: Alter database character set and collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
