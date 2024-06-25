88b42ee0a86ba305a774489245d491314a50deb2
2004Re1 <revanaxun559@gmail.com>
2024-06-26 00:34:18 +0400

    new commit

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 --CHANGING
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE second_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE second_table MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER database hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER table second_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER table second_table MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
