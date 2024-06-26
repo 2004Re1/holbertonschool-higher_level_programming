e8b866bbac41651959ffb79d39fd2fb8942c55ef
2004Re1 <revanaxun559@gmail.com>
2024-06-26 23:47:13 +0400

    new commit

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,10 @@
--- aisjdoiafjaf
-SELECT tv_genres.name FROM tv_shows
-INNER JOIN tv_shows_genres ON tv_show_genres.show_id = tv_shows.id
-INNER JOIN tv_shows_genres ON tv_show_genres.genre_id = tv_genres.id
+-- Assuming the database name is passed as an argument, for example 'hbtn_0d_tvshows'
+
+USE hbtn_0d_tvshows;
+
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
