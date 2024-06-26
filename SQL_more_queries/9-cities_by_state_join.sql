4fadc10924f2bd477ed54620986f477fa93ba691
2004Re1 <revanaxun559@gmail.com>
2024-06-26 22:48:25 +0400

    new commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,8 @@
--- jsaipfjaf
-SELECT cities.id AS id, cities.name AS name, state.name AS name FROM cities
-INNER JOIN state ON state.id = cities.state_id
+-- Assuming the database name is passed as an argument, for example 'hbtn_0d_usa'
+
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+INNER JOIN states ON states.id = cities.state_id
 ORDER BY cities.id ASC;
