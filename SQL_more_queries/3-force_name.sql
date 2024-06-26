87e053d89e4574a144e28b7fd2bf651b2473561d
2004Re1 <revanaxun559@gmail.com>
2024-06-26 21:51:56 +0400

    new commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,5 @@
--- CRtegagifa
-CREATE TABLE IF NOT EXISTS force_name ( id INT, name VARCHAR(256), );
+-- ajsoifja
+CREATE TABLE IF NOT EXISTS force_name (
+  id INT,
+  name VARCHAR(256) NOT NULL
+);
