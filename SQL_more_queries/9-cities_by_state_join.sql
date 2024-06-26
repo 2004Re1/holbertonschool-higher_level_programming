ecd3cd9b762acea0a6b7c687cc11d97264d4f527
2004Re1 <revanaxun559@gmail.com>
2024-06-26 22:46:18 +0400

    new commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- jsaipfjaf
-SELECT cities.id, cities.name, state.name FROM cities
+SELECT cities.id AS id, cities.name AS name, state.name AS name FROM cities
 INNER JOIN state ON state.id = cities.state_id
 ORDER BY cities.id ASC;
