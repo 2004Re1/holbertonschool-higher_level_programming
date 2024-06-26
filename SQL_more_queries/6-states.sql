7aec2f2cdbe5a06fa0b12c17315908b28517e2b4
2004Re1 <revanaxun559@gmail.com>
2024-06-26 22:00:48 +0400

    new commit

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,11 @@
--- CREATING DATABASE
+-- Create the database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---TABEL CREATING
-USE hbtn_0d_usa
---NOW
+
+-- Use the database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Create the table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
-	id INT,
-	name VARCHAR(256),
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
 );
