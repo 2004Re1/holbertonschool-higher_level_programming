93c9e28bf4915ae8ecaacad41245cc47a861f54c
2004Re1 <revanaxun559@gmail.com>
2024-06-26 22:51:11 +0400

    new commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,5 @@
--- Query to list all cities with their corresponding state names
-
-SELECT
-    cities.id,
-    cities.name,
-    (SELECT name FROM states WHERE id = cities.state_id) AS state_name
-FROM
-    cities
-ORDER BY
-    cities.id ASC;
+-- List all cities along with their states
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
