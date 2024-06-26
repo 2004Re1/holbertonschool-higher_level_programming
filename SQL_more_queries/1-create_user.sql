1a9611b6ac39f69462fcb4818e0e03e2d057576d
2004Re1 <revanaxun559@gmail.com>
2024-06-26 21:40:15 +0400

    new commit

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- asjfaifja
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+-- aksdakf
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
