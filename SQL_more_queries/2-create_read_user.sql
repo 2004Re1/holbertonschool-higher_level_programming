d0153929eb74be4dfefcdf682d68fdd91e7144cf
2004Re1 <revanaxun559@gmail.com>
2024-06-26 21:48:53 +0400

    new commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,3 +4,5 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 --ADDING PRIVILEHE
 GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+-- askdaf
+FLUSH PRIVILEGES;
