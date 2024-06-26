8b08f66b2fa6ceab44b06612028bf6fd54c9adfd
2004Re1 <revanaxun559@gmail.com>
2024-06-26 22:12:45 +0400

    new commit

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,13 @@
---CREATING DATABASE
+-- Create the database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---USING IT
-USE hbtn_0d_usa
---creating table
-CREATE TABLE IF NOT EXISTS parent (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+
+-- Use the database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Create the table cities if it does not exist
+CREATE TABLE IF NOT EXISTS cities (
+  id INT AUTO_INCREMENT PRIMARY KEY,
   state_id INT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  FOREIGN KEY (state_id) REFERENCE state(id)
+  name VARCHAR(256) NOT NULL,
+  FOREIGN KEY (state_id) REFERENCES states(id)
 );
