e3eb66e85208b343d29df6b300d2b6ad77cc635b
2004Re1 <revanaxun559@gmail.com>
2024-06-27 23:39:38 +0400

    new commit

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,12 @@
--- ASJDJKAF
-SELECT name FROM tv_genres
-WHERE id NOT IN ( 
-	SELECT tv_show_genres.genre_id FROM tv_show_genres
-	INNER JOIN tv_shows ON tv_show.id = tv_show_genres.show_id
-	WHERE tv_shows.title = 'Dexter'
+-- Assuming the database name is passed as an argument, for example 'hbtn_0d_tvshows'
+
+SELECT name 
+FROM tv_genres 
+WHERE id NOT IN (
+    SELECT genre_id 
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY name ASC;
+
