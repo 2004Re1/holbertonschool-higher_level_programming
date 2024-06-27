8666815aa04b11a4c60e185e9c0f8985b78fc0b7
2004Re1 <revanaxun559@gmail.com>
2024-06-27 23:57:52 +0400

    new commit

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- sakjpodakf
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv.shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
