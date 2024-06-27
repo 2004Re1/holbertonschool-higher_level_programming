a9ab86de434d8512f255b690ec9dd4051528acd6
2004Re1 <revanaxun559@gmail.com>
2024-06-28 00:08:29 +0400

    new commit

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,15 @@
 -- sjafdafa
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres.name
-INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
-GROUP BY tv_genres.name
-ORDER BY rating DESC;
+
+SELECT 
+    tv_genres.name, 
+    SUM(tv_show_ratings.rate) AS rating_sum
+FROM 
+    tv_genres
+JOIN 
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN 
+    tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY 
+    tv_genres.name
+ORDER BY 
+    rating_sum DESC;
