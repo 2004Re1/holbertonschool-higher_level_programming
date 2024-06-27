dd7e853b7694a7b04cf56575410e4b151bf34707
2004Re1 <revanaxun559@gmail.com>
2024-06-27 23:42:19 +0400

    new commit

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -5,7 +5,7 @@ FROM tv_shows
 WHERE id NOT IN (
     SELECT show_id
     FROM tv_show_genres
-    JOIN tv_genres ON tv_show_genres.show_id = tv_genres.id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY name ASC;
