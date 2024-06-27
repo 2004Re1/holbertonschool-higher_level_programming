0809e84316bfb0b0ac1edb406f7796c51773806e
2004Re1 <revanaxun559@gmail.com>
2024-06-27 23:38:41 +0400

    new commit

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,8 @@
--- List all genres of the show Dexter from hbtn_0d_tvshows
-SELECT tv_genres.name FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title != 'Dexter' 
-ORDER BY tv_genres.name ASC;
+-- ASJDJKAF
+SELECT name FROM tv_genres
+WHERE id NOT IN ( 
+	SELECT tv_show_genres.genre_id FROM tv_show_genres
+	INNER JOIN tv_shows ON tv_show.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
